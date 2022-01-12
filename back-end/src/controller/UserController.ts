a5621aa51b31e576626e505eb205d43b36cda170
Odetyte <odetamaslovaite@yahoo.com>
2022-01-12 21:52:37 +0100

    create user controller actions to create a new user, list all users and create jam-session through user

--- back-end/src/controller/UserController.ts
+++ back-end/src/controller/UserController.ts
@@ -1,26 +1,66 @@
-import {getRepository} from "typeorm";
-import {NextFunction, Request, Response} from "express";
-import {User} from "../entity/User";
+import { getRepository } from "typeorm";
+import { Request, Response } from "express";
+import { User } from "../entity/User";
+import { JamSession } from "../entity/JamSession";
 
-export class UserController {
+export default class UserController {
+  static listAll = async (request: Request, response: Response) => {
+    const userRepository = getRepository(User);
+    const users = await userRepository.find({ relations: ["jamSessions"] });
+    response.json(users);
+  };
 
-  private userRepository = getRepository(User);
+  static newUser = async (request: Request, response: Response) => {
+    let { username, email, bandRole, password } = request.body;
 
-  async all(request: Request, response: Response, next: NextFunction) {
-    return this.userRepository.find();
-  }
+    let user = new User();
+    user.username = username;
+    user.email = email;
+    user.bandRole = bandRole;
+    user.password = password;
 
-  async one(request: Request, response: Response, next: NextFunction) {
-    return this.userRepository.findOne(request.params.id);
-  }
+    const userRepository = getRepository(User);
 
-  async save(request: Request, response: Response, next: NextFunction) {
-    return this.userRepository.save(request.body);
-  }
+    // check if the user already in the database
+    const emailExist = await userRepository.findOne({ email: email });
 
-  async remove(request: Request, response: Response, next: NextFunction) {
-    let userToRemove = await this.userRepository.findOne(request.params.id);
-    if (!userToRemove) throw new Error('User not found');
-    await this.userRepository.remove(userToRemove);
-  }
+    if (emailExist)
+      return response.status(400).send("User with such email already exsits.");
+
+    try {
+      const results = await userRepository.save(user);
+      return response.send({
+        userId: user.id,
+        username: user.username,
+        bandRole: user.bandRole,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  static createJamSession = async (request: Request, response: Response) => {
+    const id = request.params;
+
+    //Get the user from database
+    const userRepository = getRepository(User);
+
+    const user = await userRepository.findOneOrFail(id);
+
+    let { title, bandRoleParticipants, location } = request.body;
+    let jamSession = new JamSession();
+    jamSession.title = title;
+    jamSession.location = location;
+    jamSession.bandRoleParticipants = bandRoleParticipants;
+    jamSession.host = user;
+
+    const jamRepository = getRepository(JamSession);
+
+    try {
+      const results = await jamRepository.save(jamSession);
+      return response.send(results);
+    } catch (error) {
+      response.status(400).send("Something went wrong");
+    }
+  };
 }
