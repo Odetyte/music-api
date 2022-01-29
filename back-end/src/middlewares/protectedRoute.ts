19422418f475ed96296fc20090e3fe11bfc1c745
Odetyte <odetamaslovaite@yahoo.com>
2022-01-29 19:29:13 +0100

    rewrite JWT token verification through Bearer token authorization

--- back-end/src/middlewares/protectedRoute.ts
+++ back-end/src/middlewares/protectedRoute.ts
@@ -1,19 +1,43 @@
 import * as jwt from "jsonwebtoken";
 import { Response, Request, NextFunction } from "express";
 
+export interface TokenInterface {
+  user: {
+    userId: number;
+  };
+}
 export function protectedRoute(
   request: Request,
   response: Response,
   next: NextFunction
 ) {
-  const token = request.header("auth-token");
-  if (!token) return response.status(401).send("Access Denied");
+  let token = request.headers["authorization"];
 
-  try {
-    const verified = jwt.verify(token, process.env.TOKEN_SECRET);
-    request.body.user = verified;
-    next();
-  } catch (error) {
-    response.status(400).send("Invalid Token");
+  if (token) {
+    if (token.startsWith("Bearer ")) {
+      // Remove Bearer from string
+      token = token.slice(7, token.length);
+    }
+
+    jwt.verify(
+      token,
+      process.env.TOKEN_SECRET,
+      (error: any, decoded: TokenInterface) => {
+        if (error) {
+          return response.json({
+            success: false,
+            message: "Token is not valid",
+          });
+        } else {
+          request.body.decoded = decoded;
+          next();
+        }
+      }
+    );
+  } else {
+    return response.json({
+      success: false,
+      message: "Auth token is not supplied",
+    });
   }
 }
