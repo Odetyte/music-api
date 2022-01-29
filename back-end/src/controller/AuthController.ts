427800b33f673c81fc5f36350389deb4ef58b9c8
Odetyte <odetamaslovaite@yahoo.com>
2022-01-29 18:43:12 +0100

    standart use of JWT

--- back-end/src/controller/AuthController.ts
+++ back-end/src/controller/AuthController.ts
@@ -1,26 +1,32 @@
 import { getRepository } from "typeorm";
-import { Request, Response, NextFunction } from "express";
+import { Request, Response } from "express";
 import { User } from "../entity/User";
 import * as jwt from "jsonwebtoken";
 
 export default class AuthController {
   static login = async (request: Request, response: Response) => {
     //Check if username and password are set
-    let { email, password } = request.body;
-
-    //Get user from database
-    const userRepository = getRepository(User);
-    let user: User;
     try {
+      let { email, password } = request.body;
+
+      //Get user from database
+      const userRepository = getRepository(User);
+      let user: User;
+
+      // find user in databse
       user = await userRepository.findOneOrFail({ where: { email, password } });
+
+      // check if user exsist
+      if (user) {
+        // if yes create JWT token which expoires afte 1 hours
+        const token = jwt.sign({ userId: user.id }, process.env.TOKEN_SECRET, {
+          expiresIn: "1h",
+        });
+        // send response with an authorization token
+        response.status(200).json({ authToken: token });
+      }
     } catch (error) {
-      response.status(401).send("Email or password is incorrect");
+      response.status(400).send("Email or password is incorrect");
     }
-
-    const token = jwt.sign({ userId: user.id }, process.env.TOKEN_SECRET, {
-      expiresIn: "1h",
-    });
-
-    response.header("auth-token", token).send(token);
   };
 }
